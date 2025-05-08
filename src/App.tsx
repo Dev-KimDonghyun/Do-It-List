8e8a618d6cb44517c13a7a0f1010a339710126f9
Dev-KimDonghyun <hyungus723@gmail.com>
2025-05-08 20:45:15 +0900

    feat: Setup Base Layout in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
+import TaskInput from './components/TaskInput';
+import TaskList from './components/TaskList';
 import './index.css';
 
 function App() {
 
   return (
-    <>
-      <h1>Start Develop!</h1>
-    </>
+
+    <div>
+      <div>
+        <header>
+          <h1>Do It! List</h1>
+        </header>
+        <main>
+          <div>
+            <TaskInput/>
+          </div>
+          <div>
+            <TaskList/>
+          </div>
+        </main>
+        <footer>
+          <p>&copy; 2025 Dev-KimDonghyun. All rights reserved.</p>
+        </footer>
+      </div>
+    </div>
+
   );
+
 };
 
 export default App;
