c8275ac9530576a5215cb5d8cc12ca2cdcf82a54
Dev-KimDonghyun <hyungus723@gmail.com>
2025-05-15 09:14:46 +0900

    feat: Apply TailwindCSS Styling to Footer of App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -118,7 +118,7 @@ function App() {
             />
           </div>
         </main>
-        <footer>
+        <footer className="w-full h-16 flex fixed flex-col text-md md:text-lg gap-y-1 md:gap-y-2 bottom-0 text-center justify-center items-center bg-gray-600 text-white">
           <p>&copy; 2025 Dev-KimDonghyun. All rights reserved.</p>
         </footer>
       </div>
