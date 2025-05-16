09eb96debc422a3cd64e5a99af6f2c250ef31352
Dev-KimDonghyun <hyungus723@gmail.com>
2025-05-16 22:40:00 +0900

    feat: Apply TailwindCSS Styling to TaskInput.tsx

--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -16,9 +16,10 @@ const TaskInput = ({
     inputRef.current?.focus();
   }, []);
   return (
-    <div>
-      <form onSubmit={uploadToList}>
+    <div className="mt-2 mb-10">
+      <form className="justify-center items-center" onSubmit={uploadToList}>
         <input
+          className="align-middle w-45 md:w-90 h-8 md:h-10 bg-sky-200 text-lg md:text-xl text-center text-sky-900 rounded-xl md:rounded-2xl mr-2 shadow-inner shadow-sky-300 hover:scale-101 transition-transform duration-300 cursor-text"
           ref={inputRef}
           onChange={(e) => {
             setToDoInput(e.target.value);
@@ -27,7 +28,12 @@ const TaskInput = ({
           value={toDoInput}
           required
         />
-        <button type="submit">Button</button>
+        <button
+          className="align-middle w-20 md:w-30 h-8 md:h-10 bg-sky-300 text-sm md:text-xl text-sky-800 rounded-xl md:rounded-2xl md:shadow shadow-sky-400 cursor-pointer hover:scale-98 transition-transform duration-300"
+          type="submit"
+        >
+          Button
+        </button>
       </form>
     </div>
   );
