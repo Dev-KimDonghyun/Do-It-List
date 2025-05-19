08ec1f520b02d91aa9cd54ec3e8ecd83558c949a
Dev-KimDonghyun <hyungus723@gmail.com>
2025-05-19 13:14:34 +0900

    feat: Apply TailwindCSS Styling to TaskItem.tsx

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -11,24 +11,45 @@ type TaskItem = {
 
 const TaskItem = ({ todo, deleteToDo, compeleteToDo }: TaskItem) => {
   return (
-    <div>
-      <li key={todo.id}>
-        {todo.content}
-        {todo.didIt ? <span>Done</span> : <span>Doing</span>}
-        <button
-          onClick={() => {
-            compeleteToDo(todo.id);
-          }}
-        >
-          {todo.didIt ? <span>Undo</span> : <span>Done</span>}
-        </button>
-        <button
-          onClick={() => {
-            deleteToDo(todo.id);
-          }}
-        >
-          Delete
-        </button>
+    <div className="mt-8 flex text-center items-center justify-center">
+      <li
+        className="bg-sky-200 gap-y-2 md:gap-y-0 w-72 h-27 md:w-160 md:h-14 rounded-3xl flex flex-col md:flex-row text-center items-center justify-center"
+        key={todo.id}
+      >
+        <span className="w-[90%] md:w-[40%] right text-4xl text-sky-800 mx-auto truncate block">
+          {todo.content}
+        </span>
+        <div className="bg-sky-300 flex text-center items-center justify-center w-68 md:w-76 h-10 rounded-2xl md:mr-2">
+          {todo.didIt ? (
+            <span className="text-2xl text-green-500">Done</span>
+          ) : (
+            <span className="text-2xl text-yellow-500">Doing</span>
+          )}
+          <button
+            className="mx-2 text-md px-1 rounded-lg bg-blue-200 cursor-pointer"
+            onClick={() => {
+              compeleteToDo(todo.id);
+            }}
+          >
+            {todo.didIt ? (
+              <span className="text-md text-yellow-500 cursor-pointer">
+                Undo
+              </span>
+            ) : (
+              <span className="text-md text-green-500 cursor-pointer">
+                Done
+              </span>
+            )}
+          </button>
+          <button
+            className="text-md text-red-500 px-1 rounded-lg bg-blue-200 cursor-pointer"
+            onClick={() => {
+              deleteToDo(todo.id);
+            }}
+          >
+            Delete
+          </button>
+        </div>
       </li>
     </div>
   );
